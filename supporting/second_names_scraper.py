92af20224abb1bd7260ab8f81d8a25233b091717
Andreas Rubik <rubikandreas@gmail.com>
2018-12-28 10:08:54 +0100

    moved parsing to own function

--- supporting/second_names_scraper.py
+++ supporting/second_names_scraper.py
@@ -1,13 +1,14 @@
 import requests
 from bs4 import BeautifulSoup
 
-url_without_page = "https://namenskarten.lima-city.at/oesterreichs-haeufigste-nachnamen.php?p="
-with open('second_names.txt', 'w') as output:
+def parse_names():
+    url_without_page = "https://namenskarten.lima-city.at/oesterreichs-haeufigste-nachnamen.php?p="
+    namelist = ""
     for i in range(100):
         url = url_without_page + str(i)
         print("Opening URL: " + url)
-        req = requests.get(url)
-        parsed_page = BeautifulSoup(req.text, "html.parser")
+        resp = requests.get(url)
+        parsed_page = BeautifulSoup(resp.text, "html.parser")       
         for row in parsed_page.findAll("table")[0].findAll("tr"):
             tds = row.findAll("td")
             if not tds:
@@ -15,3 +16,7 @@ with open('second_names.txt', 'w') as output:
             name_in_column = BeautifulSoup(str(tds[1].contents[0]), "html.parser").findAll("a")[0].text
             print(name_in_column)
             print(name_in_column, file=output, sep=" ", end=" ")
+    return namelist
+
+with open('second_names.txt', 'w') as output:
+    print(parse_names(), file=output, sep=" ", end=" ")
